1aff7fdf645a7741cd16142c554fe1a7d6e87bfc
Alisha Mahanty <alishamahanty0805@gmail.com>
2025-08-21 12:52:32 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,5 +1,24 @@
-http://localhost:3000/events
+import Link from 'next/link';
 
-http://localhost:3000/rsvp
-
-
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Pixabeam Events App</h1>
+      <p>
+        Check out the list of upcoming events or RSVP to an event.
+      </p>
+      <ul>
+        <li>
+          <Link href="/events">
+            Go to Events Page
+          </Link>
+        </li>
+        <li>
+          <Link href="/rsvp">
+            Go to RSVP Page
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
